cd62769f0ad23577024828cb4752e28bbf154335
TatianaDChiacchio <tatachiacchio@gmail.com>
2022-06-28 17:59:27 -0300

    aula do dia 28-06

--- Servicos/src/main/java/com/soulcode/Servicos/Controllers/ChamadoController.java
+++ Servicos/src/main/java/com/soulcode/Servicos/Controllers/ChamadoController.java
@@ -3,9 +3,11 @@ package com.soulcode.Servicos.Controllers;
 import com.soulcode.Servicos.Models.Chamado;
 import com.soulcode.Servicos.Services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin
@@ -27,5 +29,30 @@ public class ChamadoController {
         Chamado chamado  = chamadoService.mostrarUmChamado(idChamado);
         return ResponseEntity.ok().body(chamado);
 
+    }
+
+    @GetMapping("/chamadosPeloCliente/{idCliente}")
+    public List<Chamado> buscarChamadosPeloCliente(@PathVariable Integer idCliente){
+        List<Chamado> chamados = chamadoService.buscarChamadosPeloCliente(idCliente);
+        return chamados;
+    }
+
+    @GetMapping("/chamadosPeloFuncionario/{idFuncionario}")
+    public List<Chamado> buscarChamadosPeloFuncionario(@PathVariable Integer idFuncionario){
+        List<Chamado> chamados = chamadoService.buscarChamadosPeloFuncionario(idFuncionario);
+        return chamados;
+    }
+
+    @GetMapping("/chamadosPeloStatus")
+    public List<Chamado> buscarChamadosPeloStatus(@RequestParam("status") String status){
+        List<Chamado> chamados = chamadoService.buscarChamadosPeloStatus(status);
+        return chamados;
+    }
+
+    @GetMapping("/chamadosPorIntervaloData")
+    public List<Chamado> buscarPorIntervaloData(@RequestParam("data1") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date data1,
+                                                @RequestParam("data2") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date data2){
+        List<Chamado> chamados = chamadoService.buscarPorIntervaloData(data1,data2);
+        return chamados;
     }
 }
--- Servicos/src/main/java/com/soulcode/Servicos/Services/ChamadoService.java
+++ Servicos/src/main/java/com/soulcode/Servicos/Services/ChamadoService.java
@@ -1,10 +1,15 @@
 package com.soulcode.Servicos.Services;
 
 import com.soulcode.Servicos.Models.Chamado;
+import com.soulcode.Servicos.Models.Cliente;
+import com.soulcode.Servicos.Models.Funcionario;
 import com.soulcode.Servicos.Repositories.ChamadoRepository;
+import com.soulcode.Servicos.Repositories.ClienteRepository;
+import com.soulcode.Servicos.Repositories.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +19,12 @@ public class ChamadoService {
     @Autowired
     ChamadoRepository chamadoRepository;
 
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Autowired
+    FuncionarioRepository funcionarioRepository;
+
     // findAll (método da Spring Data) - busca todos os registros
     public List<Chamado> mostrarTodosChamados(){
         return chamadoRepository.findAll();	}
@@ -22,5 +33,24 @@ public class ChamadoService {
     public Chamado mostrarUmChamado(Integer idChamado) {
         Optional<Chamado> chamado = chamadoRepository.findById(idChamado);
         return chamado.orElseThrow();
+    }
+
+    public List<Chamado> buscarChamadosPeloCliente(Integer idCliente){
+        Optional<Cliente> cliente = clienteRepository.findById(idCliente);
+        return chamadoRepository.findByCliente(cliente);
+
+    }
+
+    public List<Chamado> buscarChamadosPeloFuncionario(Integer idFuncionario){
+        Optional<Funcionario> funcionario = funcionarioRepository.findById(idFuncionario);
+        return chamadoRepository.findByFuncionario(funcionario);
+    }
+
+    public List<Chamado> buscarChamadosPeloStatus(String status){
+        return chamadoRepository.findByStatus(status);
+    }
+
+    public List<Chamado> buscarPorIntervaloData(Date data1, Date data2){
+        return chamadoRepository.findByIntervaloData(data1,data2);
     }
 }
