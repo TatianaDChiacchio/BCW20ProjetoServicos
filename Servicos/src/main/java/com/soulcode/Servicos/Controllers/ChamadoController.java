c38488cb3db34285b28ae159b70dc68c40ef3656
TatianaDChiacchio <tatachiacchio@gmail.com>
2022-06-29 17:57:33 -0300

    aula do dia 29-06

--- Servicos/src/main/java/com/soulcode/Servicos/Controllers/ChamadoController.java
+++ Servicos/src/main/java/com/soulcode/Servicos/Controllers/ChamadoController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Date;
 import java.util.List;
 
@@ -55,4 +57,51 @@ public class ChamadoController {
         List<Chamado> chamados = chamadoService.buscarPorIntervaloData(data1,data2);
         return chamados;
     }
+
+    // aqui vamos definir o endpoint para o serviço de cadastro de um novo chamado
+    // para cadastro precisamos anotar como método http - post
+    @PostMapping("/chamados/{idCliente}")
+    public ResponseEntity<Chamado> cadastrarChamado(@PathVariable Integer idCliente,
+                                                    @RequestBody Chamado chamado){
+        chamado = chamadoService.cadastrarChamado(chamado,idCliente);
+        //nesse momento o chamado já foi cadastrado no database
+        //precisamos agora criar o caminho (uri) para que esse novo chamado possa ser acessado
+        URI novaUri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}")
+                .buildAndExpand(chamado.getIdChamado()).toUri();
+        return ResponseEntity.created(novaUri).body(chamado);
+    }
+
+    //vamos mapear o serviço de excluir um chamado
+    @DeleteMapping("/chamados/{idChamado}")
+       public ResponseEntity<Void> excluirChamado(@PathVariable Integer idChamado){
+        chamadoService.excluirChamado(idChamado);
+        return ResponseEntity.noContent().build();
+    }
+
+    // vamos mapear o serviço de editar um chamado
+    // para edição precisamos do método http do tipo put
+    @PutMapping("/chamados/{idChamado}")
+    public ResponseEntity<Chamado> editarChamado(@PathVariable Integer idChamado,
+                                                 @RequestBody Chamado chamado){
+        chamado.setIdChamado(idChamado);
+        chamadoService.editarChamado(chamado, idChamado);
+        return ResponseEntity.ok().build();
+    }
+
+    //vamos fazer o mapeamento do método de atribuir um funcionario a um determinado chamado
+    @PutMapping("/chamadosAtribuirFuncionario/{idChamado}/{idFuncionario}")
+    public ResponseEntity<Chamado> atribuirFuncionario(@PathVariable Integer idChamado,
+                                                       @PathVariable Integer idFuncionario){
+        chamadoService.atribuirFuncionario(idChamado, idFuncionario);
+        return ResponseEntity.noContent().build();
+    }
+
+    //vamos construr o mapeamento do método para modificar o status de um chamado
+    @PutMapping("/chamadosModificarStatus/{idChamado}")
+    public ResponseEntity<Chamado> modificarStatus(@PathVariable Integer idChamado,
+                                                   @RequestParam("status") String status){
+        chamadoService.modificarStatus(idChamado,status);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- Servicos/src/main/java/com/soulcode/Servicos/Services/ChamadoService.java
+++ Servicos/src/main/java/com/soulcode/Servicos/Services/ChamadoService.java
@@ -3,6 +3,7 @@ package com.soulcode.Servicos.Services;
 import com.soulcode.Servicos.Models.Chamado;
 import com.soulcode.Servicos.Models.Cliente;
 import com.soulcode.Servicos.Models.Funcionario;
+import com.soulcode.Servicos.Models.StatusChamado;
 import com.soulcode.Servicos.Repositories.ChamadoRepository;
 import com.soulcode.Servicos.Repositories.ClienteRepository;
 import com.soulcode.Servicos.Repositories.FuncionarioRepository;
@@ -53,4 +54,88 @@ public class ChamadoService {
     public List<Chamado> buscarPorIntervaloData(Date data1, Date data2){
         return chamadoRepository.findByIntervaloData(data1,data2);
     }
+
+    //cadastrar um novo chamado
+    // temos 2 regras:
+    //          1) no momento do cadastro do chamado, já devemos informar de qual cliente é
+    //          2) no momento do cadastro do chamado, a princípio vamos fazer esse cadastro
+    //              sem estar atribuido a um funcinário
+    //          3) no momento do cadastro do chamado, o status desse chamado deve ser RECEBIDO
+
+    //serviço para cadastro de novo chamado
+    public Chamado cadastrarChamado(Chamado chamado, Integer idCliente){
+        // regra 3 - atribuuição do status recebido pra o chamado que está sendo cadastrado
+        chamado.setStatus(StatusChamado.RECEBIDO);
+        // regra 2 - dizer que ainda não atribuimos esse chamado pra nenhum funcionário
+        chamado.setFuncionario(null);
+        //regra 1 - buscando os dados do cliente dono do chamado
+        Optional<Cliente> cliente = clienteRepository.findById(idCliente);
+        chamado.setCliente(cliente.get());
+        return chamadoRepository.save(chamado);
+    }
+
+    // Método para exclusão de um chamado
+    public void excluirChamado(Integer idChamado){
+        chamadoRepository.deleteById(idChamado);
+    }
+
+    //Método para editar um chamado
+    // no momento da edição de um chamado devemos preservar o cliente e o funcionário desse chamado
+    // vamos editar os dados do chamado, mas contiuamos com os dados do cliente e os dados do funcionário
+    public Chamado editarChamado(Chamado chamado, Integer idChamado){
+        //instanciamos aqui um objeto do tipo Chamado para guardar os dados do chamados
+        //sem as novas alteracoes
+        Chamado chamadoSemAsNovasAlteracoes = mostrarUmChamado(idChamado);
+        Funcionario funcionario = chamadoSemAsNovasAlteracoes.getFuncionario();
+        Cliente cliente = chamadoSemAsNovasAlteracoes.getCliente();
+
+        chamado.setCliente(cliente);
+        chamado.setFuncionario(funcionario);
+        return chamadoRepository.save(chamado);
+    }
+
+    //método para atribuir um funcionário para um determinado chamado
+    // ou trocar o funcionário de determinado chamado
+    // -> regra -> no momento em que um determinado chamado é atribuído a um funcionário
+    //             o status do chamado precisa ser alterado para ATRIBUIDO
+    public Chamado atribuirFuncionario(Integer idChamado, Integer idFuncionario){
+        // buscar os dados do funcionário que vai ser atibuído a esse chamado
+        Optional<Funcionario> funcionario = funcionarioRepository.findById(idFuncionario);
+        // buscar o chamado para o qual vai ser especificado o funcionário escolhido
+        Chamado chamado = mostrarUmChamado(idChamado);
+        chamado.setFuncionario(funcionario.get());
+        chamado.setStatus(StatusChamado.ATRIBUIDO);
+
+        return chamadoRepository.save(chamado);
+    }
+
+    //método para modificar o status de um chamado
+    public Chamado modificarStatus(Integer idChamado,String status){
+        Chamado chamado = mostrarUmChamado(idChamado);
+        switch (status){
+            case "ATRIBUIDO":
+            {
+                chamado.setStatus(StatusChamado.ATRIBUIDO);
+                break;
+            }
+            case "CONCLUIDO":
+            {
+                chamado.setStatus(StatusChamado.CONCLUIDO);
+                break;
+            }
+            case "ARQUIVADO":
+            {
+                chamado.setStatus(StatusChamado.ARQUIVADO);
+                break;
+            }
+            case "RECEBIDO":
+            {
+                chamado.setStatus(StatusChamado.RECEBIDO);
+                break;
+            }
+        }
+        return chamadoRepository.save(chamado);
+    }
+
+
 }
